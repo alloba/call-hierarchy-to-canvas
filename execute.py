88aa83d6d66befa4c3bed45b0b6009a4103b03de
Alex Bates <Alex.Bates@nielseniq.com>
2023-10-24 17:34:07 -0400

    add argparse for cli execution

--- execute.py
+++ execute.py
@@ -1,5 +1,9 @@
+import sys
+
 from graphviz_operations import plain_dot_to_canvas, tree_to_graphviz_rawtext, render_dot
 from treegen import parse_text_to_tree
+
+import argparse
 
 
 def call_hierarchy_to_canvas(file_path: str):
@@ -15,6 +19,13 @@ def call_hierarchy_to_canvas(file_path: str):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Convert call hierarchy to obsidian canvas')
+    parser.add_argument('-f', '--file-path', type=str, required=True, help='path to call hierarchy file. must be space-indented text file.')
+    try:
+        args = parser.parse_args()
+        v = call_hierarchy_to_canvas(args.file_path)
+        print(v)
+    except TypeError as e:
+        parser.print_help()
 
-    v = call_hierarchy_to_canvas('sample-call-graph.txt')
-    print(v)
+
